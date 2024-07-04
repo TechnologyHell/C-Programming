3ef85ed6e176911b1e84feaa8aa342a99a618484
TechnologyHell <nitishsinghslg@gmail.com>
2024-07-04 15:05:24 +0530

    update mul

--- 5_arrays2D/matrixMultiplication.c
+++ 5_arrays2D/matrixMultiplication.c
@@ -3,55 +3,51 @@
 
 int main()
 {
-    int a[max][max], b[max][max], res[max][max], i, j, k, size;
+    int a[max][max], b[max][max], res[max][max], i, j, k, row, col;
 
-    printf("Enter the order of the matrices : ");
-    scanf("%d", &size);
+    printf("Enter the order for a square matrix : ");
+    scanf("%d %d", &row, &col);
 
     printf("Enter matrix A : \n");
-    for(i=0; i<size; i++)
+    for(i=0; i<row; i++)
     {
-        for(j=0; j<size; j++)
+        for(j=0; j<col; j++)
         {
             scanf("%d", &a[i][j]);
         }
     }
 
     printf("Enter matrix B : \n");
-    for(i=0; i<size; i++)
+    for(i=0; i<row; i++)
     {
-        for(j=0; j<size; j++)
+        for(j=0; j<col; j++)
         {
             scanf("%d", &b[i][j]);
         }
     }
-    
-    for(i=0; i<size; i++)
+
+    for(i=0; i<row; i++)
     {
-        for(j=0; j<size; j++)
+        for(j=0; j<col; j++)
         {
             res[i][j] = 0;
-        }
-    }
-
-    for(i=0; i<size; i++)
-    {
-        for(j=0; j<size; j++)
-        {
-            for(k=0; k<size; k++)
+            for(k=0; k<col; k++)
             {
-                res[i][j] = res[i][j] + a[i][k] * b[j][k];
+                res[i][j] = res[i][j] + (a[i][k] * b[k][j]);
             }
         }
     }
 
     printf("Resultant Matrix : \n");
-    for(i=0; i<size; i++)
+    for(i=0; i<row; i++)
     {
-        for(j=0; j<size; j++)
+        for(j=0; j<col; j++)
         {
             printf("%d\t", res[i][j]);
         }
         printf("\n");
     }
+
+    return 0;
+    
 }
